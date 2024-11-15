3c12f71bdce0bfeacfc16b7d9fc67a0a6959e621
Simon <simonmansson@live.se>
2024-11-15 19:52:51 +0100

    Inlämning

--- game.js
+++ game.js
@@ -1,10 +1,9 @@
-
-console.log(localStorage)
-
-
+console.log(localStorage);
 
 function firstScene() {
+  console.log("12123");
   location.href = "firstscene.html";
+  document.getElementById("dialogBoxContainer").style.display = "none";
 }
 
 let haveNote = false;
@@ -19,10 +18,10 @@ let inventory = JSON.parse(localStorage.getItem("inventoryContainer")) || [];
 
 function updateInventoryDisplay() {
   if (inventory.includes("key")) {
-      document.getElementById("keyIcon").style.display = "block";
+    document.getElementById("keyIcon").style.display = "block";
   }
   if (inventory.includes("note")) {
-      document.getElementById("noteIcon").style.display = "block";
+    document.getElementById("noteIcon").style.display = "block";
   }
   if (inventory.includes("gold")) {
     document.getElementById("goldenChestIcon").style.display = "block";
@@ -35,7 +34,8 @@ function updateInventoryDisplay() {
 const backpack = document.getElementById("backpack");
 const inventoryContainer = document.getElementById("inventoryContainer");
 
-const originalImage = "assets/Interior/Foremal/b3b5f4da-d678-4ba2-88ed-adc93f1a31d7-removebg-preview.png";
+const originalImage =
+  "assets/Interior/Foremal/b3b5f4da-d678-4ba2-88ed-adc93f1a31d7-removebg-preview.png";
 const hoverImage = "assets/Interior/Foremal/image-removebg-preview.png";
 
 function showInventory() {
@@ -44,16 +44,15 @@ function showInventory() {
 }
 
 function closeInventory() {
-  if (inventoryContainer) 
-    inventoryContainer.style.display = "";
+  if (inventoryContainer) inventoryContainer.style.display = "";
 }
 
 function changeBackpackImage() {
-  backpack.src = hoverImage; // Byt till hoverbild vid mouseenter
+  backpack.src = hoverImage;
 }
 
 function resetBackpackImage() {
-  backpack.src = originalImage; // Återställ till originalbild vid mouseleave
+  backpack.src = originalImage;
 }
 
 backpack.addEventListener("mouseenter", () => {
@@ -66,51 +65,41 @@ backpack.addEventListener("mouseleave", () => {
   resetBackpackImage();
 });
 
-const itemPickUpIcon = document.getElementById("itemPickUpIcon")
+const itemPickUpIcon = document.getElementById("itemPickUpIcon");
 
 function collectKey() {
-  if (!inventory.includes("key")) 
-    inventory.push("key");
-    localStorage.setItem("inventoryContainer", JSON.stringify(inventory));
-    document.getElementById("key1DialogText").style.display = "block";
-    document.getElementById("dialogBoxKey1").style.display = "block";
-    document.getElementById("talkingHeadKeyDrawer").style.display = "block";
-    document.getElementById("talkingHead").style.display = "block";
-    
-  
+  if (!inventory.includes("key")) inventory.push("key");
+  localStorage.setItem("inventoryContainer", JSON.stringify(inventory));
+  document.getElementById("key1DialogText").style.display = "block";
+  document.getElementById("dialogBoxKey1").style.display = "block";
+  document.getElementById("talkingHeadKeyDrawer").style.display = "block";
+  document.getElementById("talkingHead").style.display = "block";
 }
 
 function collectNote() {
   haveNote = true;
-  if (!inventory.includes("note")) 
-    inventory.push("note");
-    localStorage.setItem("inventoryContainer", JSON.stringify(inventory));
-    document.getElementById("noteDialogText").style.display = "block";
-    document.getElementById("dialogBoxNote").style.display = "block";
-    document.getElementById("talkingHeadNoteDrawer").style.display = "block";
+  if (!inventory.includes("note")) inventory.push("note");
+  localStorage.setItem("inventoryContainer", JSON.stringify(inventory));
+  document.getElementById("noteDialogText").style.display = "block";
+  document.getElementById("dialogBoxNote").style.display = "block";
+  document.getElementById("talkingHeadNoteDrawer").style.display = "block";
 
-    document.getElementById("itemPickUpIcon").style.display = "block";
-    
-
+  document.getElementById("itemPickUpIcon").style.display = "block";
 }
 
 function collectGold() {
-  if (!inventory.includes("gold")) 
-    inventory.push("gold");
-    localStorage.setItem("inventoryContainer", JSON.stringify(inventory));
+  if (!inventory.includes("gold")) inventory.push("gold");
+  localStorage.setItem("inventoryContainer", JSON.stringify(inventory));
 }
 
 function collectBarrelKey() {
-  if (!inventory.includes("barrelKey")) 
-    inventory.push("barrelKey");
-    localStorage.setItem("inventoryContainer", JSON.stringify(inventory));
-    document.getElementById("dialogTextBarrelKey").style.display = "block";   
-    document.getElementById("dialogBoxBarrelKey").style.display = "block";
-    document.getElementById("talkingHeadBarrel").style.display = "block";
-    document.getElementById("dialogBoxContainerBarrel").style.display = "block";
-
+  if (!inventory.includes("barrelKey")) inventory.push("barrelKey");
+  localStorage.setItem("inventoryContainer", JSON.stringify(inventory));
+  document.getElementById("dialogTextBarrelKey").style.display = "block";
+  document.getElementById("dialogBoxBarrelKey").style.display = "block";
+  document.getElementById("talkingHeadBarrel").style.display = "block";
+  document.getElementById("dialogBoxContainerBarrel").style.display = "block";
 }
-
 
 let backBtn = document.getElementById("backBtn");
 
@@ -119,26 +108,21 @@ function closeDialogBox() {
 }
 
 function goBack() {
-    history.back();
+  history.back();
 }
-
-
 
 function keyDrawer() {
-  location.href = "keyDrawer.html"
+  location.href = "keyDrawer.html";
 }
-
-
 
 function showPainting() {
   location.href = "painting.html";
 }
 
 function secondScene() {
-  if (inventory.includes("key")) 
-    location.href = "secondscene.html";
+  if (inventory.includes("key")) location.href = "secondscene.html";
   else {
-    alert("Dörren är låst, det kanske finns en nyckel här någonstans?")
+    alert("Dörren är låst, det kanske finns en nyckel här någonstans?");
   }
 }
 
@@ -147,49 +131,48 @@ function toMainMenu() {
 }
 
 function secondSceneFromGoldRoom() {
-    location.href = "secondscene.html";
+  location.href = "secondscene.html";
 }
-
 
 function enterGoldenRoom() {
-  location.href = "goldroom.html"
+  location.href = "goldroom.html";
 }
 
-
 function hallwayToFrontDoor() {
-  location.href = "maindoor.html"
+  location.href = "maindoor.html";
 }
 
 function showBarrelKey() {
-  location.href = "keyBarrel.html"
-
+  location.href = "keyBarrel.html";
 }
 
-
-
 function finalDoor() {
-  if (inventory.includes("barrelKey"))
-  location.href = "lastscene.html"
+  if (inventory.includes("gold") && inventory.includes("barrelKey"))
+    location.href = "lastscene.html";
   else {
-    alert("Dörren är låst, det kanske finns en nyckel här någonstans?")
+    alert("Vi behöver hitta guldet OCH sista nyckeln!");
   }
 }
 
 function combLock() {
-  location.href = "combLock.html"
-
+  if (!inventory.includes("note"))
+    alert(
+      "Det behövs en kod för detta lås! Vi måste ha missat den någonstans."
+    );
+  else {
+    location.href = "combLock.html";
+  }
 }
 
 function openDrawer() {
-  location.href = "openDrawer.html"
-  
+  location.href = "openDrawer.html";
 }
-  
+
 //Textbox in first scene (bedroom)
-document.addEventListener("DOMContentLoaded", function() {
-  // Texten som ska visas bokstav för bokstav
-  const dialogText = "Spelet går ut på att ta sig ut ur slottet med skattkistan, och i detta rum finns det två saker du behöver för det!";
-  
+document.addEventListener("DOMContentLoaded", function () {
+  const dialogText =
+    "Spelet går ut på att ta sig ut ur slottet med skattkistan, och i detta rum finns det två saker du behöver för det!";
+
   const dialogElement = document.getElementById("dialogText");
   let charIndex = 0;
 
@@ -197,19 +180,17 @@ document.addEventListener("DOMContentLoaded", function() {
     if (charIndex < dialogText.length) {
       dialogElement.innerHTML += dialogText[charIndex];
       charIndex++;
-      setTimeout(showNextChar, 50); // Justera hastigheten här
+      setTimeout(showNextChar, 50);
     }
   }
-  
-  // Starta bokstav-för-bokstav-animationen
+
   showNextChar();
 });
 
 //Textbox in hallway
-document.addEventListener("DOMContentLoaded", function() {
-  // Texten som ska visas bokstav för bokstav
-  const dialogTextHallway = "Där nere verkar man kunna gå ut, men först ser denna dörr till vänster mycket intressant ut..";
-  
+document.addEventListener("DOMContentLoaded", function () {
+  const dialogTextHallway =
+    "Där nere verkar man kunna gå ut, men först ser denna dörr till vänster mycket intressant ut..";
   const dialogElement = document.getElementById("dialogTextHallway");
   let charIndex = 0;
 
@@ -217,19 +198,18 @@ document.addEventListener("DOMContentLoaded", function() {
     if (charIndex < dialogTextHallway.length) {
       dialogElement.innerHTML += dialogTextHallway[charIndex];
       charIndex++;
-      setTimeout(showNextChar, 50); // Justera hastigheten här
+      setTimeout(showNextChar, 50);
     }
   }
-  
-  // Starta bokstav-för-bokstav-animationen
+
   showNextChar();
 });
 
 //Textbox key 1 collected in drawer
-document.addEventListener("DOMContentLoaded", function() {
-  // Texten som ska visas bokstav för bokstav
-  const key1DialogText = "Snyggt! Du hittar en nyckel och stoppar den i din ryggsäck, undra vad den är till?";
-  
+document.addEventListener("DOMContentLoaded", function () {
+  const key1DialogText =
+    "Snyggt! Du hittar en nyckel och stoppar den i din ryggsäck, undra vad den är till?";
+
   const dialogElement = document.getElementById("key1DialogText");
   let charIndex = 0;
 
@@ -237,19 +217,18 @@ document.addEventListener("DOMContentLoaded", function() {
     if (charIndex < key1DialogText.length) {
       dialogElement.innerHTML += key1DialogText[charIndex];
       charIndex++;
-      setTimeout(showNextChar, 50); // Justera hastigheten här
+      setTimeout(showNextChar, 50);
     }
   }
-  
-  // Starta bokstav-för-bokstav-animationen
+
   showNextChar();
 });
 
 //Textbox note collected in drawer
-document.addEventListener("DOMContentLoaded", function() {
-  // Texten som ska visas bokstav för bokstav
-  const noteDialogText = "Du hittar en mystisk lapp med någon slags kod som du stoppar ner i din ryggsäck.."
-  
+document.addEventListener("DOMContentLoaded", function () {
+  const noteDialogText =
+    "Du hittar en mystisk lapp med någon slags kod som du stoppar ner i din ryggsäck..";
+
   const dialogElement = document.getElementById("noteDialogText");
   let charIndex = 0;
 
@@ -257,19 +236,18 @@ document.addEventListener("DOMContentLoaded", function() {
     if (charIndex < noteDialogText.length) {
       dialogElement.innerHTML += noteDialogText[charIndex];
       charIndex++;
-      setTimeout(showNextChar, 50); // Justera hastigheten här
+      setTimeout(showNextChar, 50);
     }
   }
-  
-  // Starta bokstav-för-bokstav-animationen
+
   showNextChar();
 });
 
 //Textbox goldroom
-document.addEventListener("DOMContentLoaded", function() {
-  // Texten som ska visas bokstav för bokstav
-  const dialogTextGoldRoom = "En skattkista! Den är låst, så vi måste få upp låset på något sätt!"
-  
+document.addEventListener("DOMContentLoaded", function () {
+  const dialogTextGoldRoom =
+    "En skattkista! Den är låst, så vi måste få upp låset på något sätt!";
+
   const dialogElement = document.getElementById("dialogTextGoldRoom");
   let charIndex = 0;
 
@@ -277,20 +255,19 @@ document.addEventListener("DOMContentLoaded", function() {
     if (charIndex < dialogTextGoldRoom.length) {
       dialogElement.innerHTML += dialogTextGoldRoom[charIndex];
       charIndex++;
-      setTimeout(showNextChar, 50); // Justera hastigheten här
+      setTimeout(showNextChar, 50);
     }
   }
-  
-  // Starta bokstav-för-bokstav-animationen
+
   showNextChar();
 });
 
-
 //Textbox opened chest room
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Texten som ska visas bokstav för bokstav
-  const dialogTextOpenedChest = "Wow! Du får upp kistan och fyller din ryggsäck med guld! "
-  
+  const dialogTextOpenedChest =
+    "Wow! Du får upp kistan och fyller din ryggsäck med guld! ";
+
   const dialogElement = document.getElementById("dialogTextOpenedChest");
   let charIndex = 0;
 
@@ -301,16 +278,17 @@ document.addEventListener("DOMContentLoaded", function() {
       setTimeout(showNextChar, 50); // Justera hastigheten här
     }
   }
-  
+
   // Starta bokstav-för-bokstav-animationen
   showNextChar();
 });
 
 //Textbox last room main door
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Texten som ska visas bokstav för bokstav
-  const dialogTextMainDoor = "Detta ser ut att vara utgången..men porten är låst!"
-  
+  const dialogTextMainDoor =
+    "Detta ser ut att vara utgången..men porten är låst!";
+
   const dialogElement = document.getElementById("dialogTextMainDoor");
   let charIndex = 0;
 
@@ -321,126 +299,75 @@ document.addEventListener("DOMContentLoaded", function() {
       setTimeout(showNextChar, 50); // Justera hastigheten här
     }
   }
-  
+
   // Starta bokstav-för-bokstav-animationen
   showNextChar();
 });
 
-
 //Textbox barrel key on collect
-document.addEventListener("DOMContentLoaded", function() {
-  // Texten som ska visas bokstav för bokstav
-  const dialogTextBarrelKey = "Denna nyckel ser ut att passa porten!"
-  
+document.addEventListener("DOMContentLoaded", function () {
+  const dialogTextBarrelKey = "Denna nyckel ser ut att passa porten!";
   const dialogElement = document.getElementById("dialogTextBarrelKey");
+
   let charIndex = 0;
 
   function showNextChar() {
     if (charIndex < dialogTextBarrelKey.length) {
       dialogElement.innerHTML += dialogTextBarrelKey[charIndex];
       charIndex++;
-      setTimeout(showNextChar, 50); // Justera hastigheten här
+      setTimeout(showNextChar, 50);
     }
   }
-  
-  // Starta bokstav-för-bokstav-animationen
+
   showNextChar();
 });
 
+let scenes = [
+  "Menu",
+  "Bedroom",
+  "Hallway",
+  "Goldroom",
+  "Goldchest",
+  "Courtyard",
+  "Barrel",
+  "Exit",
+];
 
-
-
-
-
-
-
-
-
-
-//ARRAY?
-let scenarios = ["Menu", "Bedroom", "Hallway", "Goldroom", "Courtyard", "Exit"];
-
-
-
-  
-
-
-
-/*const digits = document.querySelectorAll('.digits');// Hämta alla div-element med klassen "digits"
-
-
-function plusOne() {
-   for (let i = 0; i < digits.length; i++) { // Loop genom alla div-element
-    let currentDigit = parseInt(digits[i].textContent); // Hämta och konvertera textinnehållet till ett nummer
-    
-    currentDigit++; // Addera 1 till siffran
-
-    digits[i].textContent = currentDigit; // Uppdatera texten i div-elementet med den nya siffran
-}
-}
-
-digit1.addEventListener("click", (plusOne));
-
-
-console.log(digit1.textContent);
-console.log(currentNumber);*/
-
-
-
-
-//let allDigits = document.getElementsByClassName("digits");
 let digit1 = document.getElementById("digit1");
 let digit2 = document.getElementById("digit2");
 let digit3 = document.getElementById("digit3");
 let digit4 = document.getElementById("digit4");
 
-const correctCombination = [1, 3, 3, 7];  // Den rätta kombinationen som spelaren ska ange
-let playerInput = [];  // Array för att lagra spelarens siffror
-const digits = document.querySelectorAll('.digits'); // Hämta alla div-element med klassen "digits"
+const correctCombination = [1, 3, 3, 7];
+let playerInput = [];
+const digits = document.querySelectorAll(".digits");
 
+digits.forEach((digit, index) => {
+  digit.addEventListener("click", function () {
+    let currentDigit = parseInt(digit.textContent);
 
-digits.forEach((digit, index) => {                       // Lägg till en eventlyssnare på varje div-element
-  digit.addEventListener('click', function() {                                      
-    let currentDigit = parseInt(digit.textContent);  // Hämta och konvertera texten i det klickade elementet till ett nummer
-
-    if (currentDigit === 9) { // Om siffran är 9, sätt tillbaka den till 0, annars addera 1
+    if (currentDigit === 9) {
       currentDigit = 0;
     } else {
       currentDigit++;
     }
-   
-    digit.textContent = currentDigit;  // Uppdatera texten i det klickade elementet med den nya siffran
 
-    playerInput[index]= currentDigit; // Lägg till siffran i spelarens input-array
+    digit.textContent = currentDigit;
 
-     // Om index är mindre än längden på correctCombination, fyll på playerInput med siffran
-     if (playerInput.length < correctCombination.length) {
-      playerInput[index] = currentDigit;  // Sätt rätt index i playerInput
-     }
-     
-    if (JSON.stringify(playerInput) === JSON.stringify(correctCombination)) { // Kontrollera om spelarens input matchar den rätta kombinationen
-      location.href = "openedchest.html";
-      collectGold();      // Här kan du lägga till kod för att "låsa upp" något, t.ex. visa ett meddelande eller öppna en dörr.
+    playerInput[index] = currentDigit;
+
+    if (playerInput.length < correctCombination.length) {
+      playerInput[index] = currentDigit;
     }
 
-    console.log('Player Input:', playerInput.join(''));
-    console.log('Correct Combination:', correctCombination.join(''));
-    console.log('Clicked digit index:', index);   // Logga indexet för varje siffra som klickas
-    console.log('Current digit value:', currentDigit); // Logga värdet på den klickade siffran
+    if (JSON.stringify(playerInput) === JSON.stringify(correctCombination)) {
+      location.href = "openedchest.html";
+      collectGold();
+    }
 
+    console.log("Player Input:", playerInput.join(""));
+    console.log("Correct Combination:", correctCombination.join(""));
+    console.log("Clicked digit index:", index);
+    console.log("Current digit value:", currentDigit);
   });
-
-
-  
 });
-
-
-//const correctCombination = 1337;  // Den rätta kombinationen som spelaren ska ange
-
-
- 
-
-
-
-
-
